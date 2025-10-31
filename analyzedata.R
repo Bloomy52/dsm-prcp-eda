83ca5d51ee4df6a69ecd9f8f54ccc8ac0bd0145a
Louie Bloomberg <basis-gyros8g@icloud.com>
2025-10-31 10:13:59 -0500

    Updated the graphing code to make the axis, title/subtitle, and captions reflect the graph output better.

--- analyzedata.R
+++ analyzedata.R
@@ -12,10 +12,11 @@ graph_prcp_monthly <- function(dsmprecipdata)
     scale_y_continuous(breaks=seq(0,4,by=.5), minor_breaks = seq(0, 4, by=.25)) +
     scale_x_date(limits = c(start_date, end_date), breaks = date_breaks_seq, date_labels = "%b %d", expand = c(0,0)) +
     labs(
-      title = "Daily Precipitation in Des Moines, IA (2025)",
-      subtitle = "Timeline shows daily precipitation data from 1/1 to 8/9",
-      x = "Date",
-      y = "Precipitation (inches)"
+      title = "Line Graph of the Daily Precipitation in Des Moines, IA",
+      subtitle = "From January 1, 2025 to August 9, 2025",
+      x = "Date (by week)",
+      y = "Precipitation (inches)",
+        caption = "Data Source: National Centers for Environmental Information (NCEI) - NOAA"
     ) +
     theme_minimal() +
     theme(axis.text.x = element_text(angle = 45, hjust = 1))
